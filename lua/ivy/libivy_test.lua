e932f9a0cec0c3e6be0904cb326e270e36504a3b
Ade Attwood <adrian.attwood@marketdojo.com>
2023-04-18 19:57:52 +0100

    ci: move to the dockerhub conventional-tools update

--- lua/ivy/libivy_test.lua
+++ lua/ivy/libivy_test.lua
@@ -10,9 +10,18 @@ end)
 
 it("should find a dot file", function(t)
   local current_dir = libivy.ivy_cwd()
-  local matches = libivy.ivy_files("ci.yml", current_dir)
+  local matches = libivy.ivy_files(".github/workflows/ci.yml", current_dir)
 
-  if matches ~= ".github/workflows/ci.yml\n" then
-    t.error("Invalid matches: " .. matches)
+  local results = {}
+  for line in string.gmatch(matches, "[^\r\n]+") do
+    table.insert(results, line)
+  end
+
+  if #results ~= 2 then
+    t.error "Incorrect number of results"
+  end
+
+  if results[2] ~= ".github/workflows/ci.yml" then
+    t.error("Invalid matches: " .. results[2])
   end
 end)
